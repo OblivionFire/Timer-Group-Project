0a6c5d7366c0d194f5ed17028b235dee2072b9c9
Grace Hughes <sillygoosegrace@gmail.com>
2017-11-30 10:14:28 -0600

    All GUI pages

---
+++ MedicinePage/Med.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    internal class Med
+    {
+        string name;
+        string time;
+        public Med(object e)
+        {
+            string temp = (string)e;
+            int i = temp.IndexOf(',');
+            name = temp.Substring(0, i);
+            time = timers.Substring(i + 1);
+            return;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getTime()
+        {
+            return time;
+        }
+    }
+}
